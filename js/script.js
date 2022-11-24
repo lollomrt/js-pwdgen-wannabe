f8f457a0c554f7030c4e0fdce373f7016107475a
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-24 16:10:57 +0100

    Operazioni e variabili fatte - problema sul display "getelementbyid"

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//1 Richiesta nome
+let nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+
+//2 Richiesta cognome
+let cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome)
+
+//3 Richiesta colore preferito
+let colore_preferito = prompt("Inserisci il tuo colore preferito");
+console.log(colore_preferito)
+
+//4 Definire la variabile risultato
+
+let password = nome + cognome + colore_preferito + 21;
+console.log(password)
+
+//5 Mostrare il risultato visivamente
+
+document.getElementById(testo-password).innerHTML = "password"
