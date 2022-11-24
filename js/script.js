6498db3907802198dd2f3349808bb190d3104a9b
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-24 16:27:53 +0100

    Aggiornamento js - fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //1 Richiesta nome
-let nome = prompt("Inserisci il tuo nome");
+const nome = prompt("Inserisci il tuo nome");
 console.log(nome);
 
 //2 Richiesta cognome
-let cognome = prompt("Inserisci il tuo cognome");
+const cognome = prompt("Inserisci il tuo cognome");
 console.log(cognome)
 
 //3 Richiesta colore preferito
-let colore_preferito = prompt("Inserisci il tuo colore preferito");
+const colore_preferito = prompt("Inserisci il tuo colore preferito");
 console.log(colore_preferito)
 
 //4 Definire la variabile risultato
 
-let password = "Password:" + " " + nome + cognome + colore_preferito + 21;
+const password = nome + cognome + colore_preferito + 21;
 console.log(password)
 
 //5 Mostrare il risultato visivamente
